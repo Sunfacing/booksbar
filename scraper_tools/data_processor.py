2cdc571a98b48d05f6e02086858548351fb68987
Ko-Wei CHEN <83599072+Sunfacing@users.noreply.github.com>
2021-10-31 01:27:32 +0800

    Add: Create a list that will be scraped
    This function take category list(nomenclature) as base,
    then compare catalog to create a list for scraping.
    It can track progress if accident happens during scraping

--- scraper_tools/data_processor.py
+++ scraper_tools/data_processor.py
@@ -6,3 +6,28 @@ def mongo_insert(collection, product_list):
     collection.insert_many(product_list)
     name = str(collection)
     print(name, 'data created')
+
+
+def scan_category_for_scraping(catalog, key, category_list, category_query, key_for_hash):
+    """
+    Compare two collections: [category_list] and [catalog], to determine which category aren't scraped,
+    this function can track progress in case any incident halting scraping, and continue from last status
+    :param catalog: product catalog scraped each day, namely [catalog_tem_today]
+    :param catalog_query: grouping query for faster check
+    :param category_list: category list containing every available cateogries
+    :param category_query: filtering query for selecting specific category
+    :param key_for_hash: key for category_list to check if in catelog
+    """
+    key_list = catalog.distinct(key)
+    try: 
+        cate_list = category_list.find(category_query)
+    except: 
+        cate_list = category_list.aggregate(category_query)
+    unfinished_list = []
+    for cate in cate_list:
+        try:
+            if cate[key_for_hash] not in key_list: 
+                unfinished_list.append(cate)
+        except:
+            pass
+    return unfinished_list
