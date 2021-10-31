e457c89bffb1cf7e77cd26774241d1a11b2ca866
Ko-Wei CHEN <83599072+Sunfacing@users.noreply.github.com>
2021-11-01 00:03:53 +0800

    Fix: Add condition to allow slicing method to work

--- scraper/scraper_tools/scrapers.py
+++ scraper/scraper_tools/scrapers.py
@@ -4,7 +4,7 @@ import queue
 
 class Worker(threading.Thread):
 
-    def __init__(self, queue, worker_num, url_to_scrape, target_id_key, db_to_insert, scraper_func, insert_func):
+    def __init__(self, queue, worker_num, url_to_scrape, target_id_key, db_to_insert, scraper_func, insert_func, slicing=False):
         """
         :param worker_num: defind how many threads to use
         :param url_to_scrape: takes url prefix defined in file as constant
@@ -21,18 +21,17 @@ class Worker(threading.Thread):
         self.db_to_insert = db_to_insert
         self.scraper_func = scraper_func
         self.insert_func = insert_func
+        self.slicing = slicing
 
     def run(self):
         while self.queue.qsize() > 0:
             target_id = self.queue.get() 
-            print("Worker {}: start category {}".format(self.worker_num, target_id[self.target_id_key]))
-            self.do_job(target_id, self.url_to_scrape, self.target_id_key, self.db_to_insert, self.scraper_func, self.insert_func)
-            print("Worker {}: finish category {}".format(self.worker_num, target_id))
+            if self.slicing:
+                self.do_by_slicing_product_list(self.url_to_scrape, target_id, self.target_id_key, self.db_to_insert, self.scraper_func, self.insert_func)
+            else:   
+                self.do_by_category(target_id, self.url_to_scrape, self.target_id_key, self.db_to_insert, self.scraper_func, self.insert_func)
 
     def do_by_category(self, target_id, url_to_scrape, target_id_key, db_to_insert, scraper_func, insert_func):
-        """
-        This method set each user to scrape by category, so the result for each category would all or nothing
-        """
         try:
             catalog = scraper_func(url_to_scrape, target_id[target_id_key])
         except Exception as e:
@@ -42,7 +41,8 @@ class Worker(threading.Thread):
             insert_func(db_to_insert, catalog)
         except Exception as e:
             print('{} has problem with inserting data: {}'.format(target_id, e))
-   
+
+
     def do_by_slicing_product_list(self, url_to_scrape, sliced_list, target_id_key, db_to_insert, scraper_func, insert_func):
         catalog = scraper_func(url_to_scrape, sliced_list, target_id_key)
         try:
@@ -50,23 +50,34 @@ class Worker(threading.Thread):
                 insert_func(db_to_insert, catalog)
             print('Worker {} has {} done'.format(self.worker_num, len(catalog)))
         except Exception as e:
-            print('Worker {} has problem with inserting data: {}'.format(self.worker_num, e))    
+            print('Worker {} has problem with inserting data: {}'.format(self.worker_num, e))   
 
 
-def multi_scrapers(worker_num, list_to_scrape, url_to_scrape, target_id_key, db_to_insert, scraper_func, insert_func):
-    """
-    Set up worker_num provided, and pass all arguments in to Worker class
-    """     
-    my_queue = queue.Queue()
-    for item in list_to_scrape:
+def multi_scrapers(worker_num, list_to_scrape, url_to_scrape, target_id_key, db_to_insert, scraper_func, insert_func, slicing=False):
+    if slicing:
+        my_queue = queue.Queue()
+        share = len(list_to_scrape) // 50
+        if share != 0:
+            for i in range(share):
+                start = i * 50
+                end = (i + 1) * 50
+                item = list_to_scrape[start: end]    
+                my_queue.put(item)
+            item = list_to_scrape[end: ]   
+        else:
+            item = list_to_scrape[share: ]
         my_queue.put(item)
+    else:
+        my_queue = queue.Queue()
+        for item in list_to_scrape:
+            my_queue.put(item)
 
     worker_list = []
     for i in range(worker_num):
         worker_list.append('worker{}'.format(i + 1))
 
     for i in range(len(worker_list)):
-        worker_list[i] = Worker(my_queue, i + 1, url_to_scrape, target_id_key, db_to_insert, scraper_func, insert_func)
+        worker_list[i] = Worker(my_queue, i + 1, url_to_scrape, target_id_key, db_to_insert, scraper_func, insert_func, slicing)
 
     for i in range(len(worker_list)):
         worker_list[i].start()
