7c89554231d868cd1bd9b9230d3b8686c1e1eb09
Ko-Wei CHEN <83599072+Sunfacing@users.noreply.github.com>
2021-11-24 16:58:41 +0800

    Refactor: Remove api / Create util
    Api and views are mixed now, so move api to another file
    There are also some repetitive functions, create functions to
    be called and use Enum to avoid magic numbers

---
+++ flaskapp/server/controller/util.py
@@ -0,0 +1,37 @@
+from collections import defaultdict
+
+from enum import Enum
+
+
+class Platform(Enum):
+    KINGSTONE = 1
+    ESLITE = 2
+    MOMO = 3
+
+
+
+
+
+def create_booslist_by_category(*returned_booklists):
+    product_list = defaultdict(dict)
+    duplicate_hash = defaultdict(dict)
+    count = 0
+    for booklist in returned_booklists:
+        for book in booklist:
+            isbn_id = book['isbn_id']
+            categroy = book['category']
+            if not duplicate_hash[isbn_id]:
+                data = {
+                'isbn_id': isbn_id,
+                'title': book['title'],
+                'cover_photo': book['cover_photo'],
+                'author': book['author'],
+                'description': book['description'],
+                }
+                if not product_list[categroy]:
+                    product_list[categroy] = [data]
+                else:
+                    product_list[categroy].append(data)
+                duplicate_hash[isbn_id] = 1
+                count += 1
+    return [product_list, count]
