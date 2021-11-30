a743d0140d7c33b2e3a574251803c013b3a53ed0
Ko-Wei CHEN <83599072+Sunfacing@users.noreply.github.com>
2021-12-01 01:03:32 +0800

    Refactor: Separate css and js from search.html

---
+++ flaskapp/server/static/search.js
@@ -0,0 +1,25 @@
+function loading(){
+    $("#loading").show();
+    $("#content").hide();       
+}
+
+
+function blockDisplay(){
+    document.getElementById('id01').style.display='block'
+}
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
