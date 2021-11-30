8a6d67ea25215904daafe92f4dd70ed8185179c7
Ko-Wei CHEN <83599072+Sunfacing@users.noreply.github.com>
2021-12-01 01:37:19 +0800

    Fix: side navlog hover cancel background color

--- flaskapp/server/static/base.js
+++ flaskapp/server/static/base.js
@@ -10,11 +10,6 @@ function closeNav() {
 }
 
 
-//Get the button
-var mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
